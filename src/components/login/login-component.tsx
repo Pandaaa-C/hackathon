92994c743da05c669354f7fb43de60c1ca6bf5c8
Pandaaa-C <pandacodeyt@gmail.com>
2024-06-04 23:05:38 +0100

    start login system

---
+++ src/components/login/login-component.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import {cn} from "@/lib/utils";
+import LoginForm from "@/components/login/login-form";
+
+export default function LoginComponent() {
+
+    return (
+        <div className={cn("w-full h-screen flex items-center justify-center")}>
+            <div className={cn("w-[350px] h-[350px] bg-black flex flex-col rounded p-1")}>
+                <div className={cn("w-full h-[50px] flex justify-center items-center")}>
+                    <p className={cn("font-bold text-2xl")}>Control Panel</p>
+                </div>
+                <LoginForm />
+            </div>
+        </div>
+    )
+}
