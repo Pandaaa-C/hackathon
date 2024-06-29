12ecbacccfefcd95f4c0460a1d787af107cf9e32
Pandaaa-C <pandacodeyt@gmail.com>
2024-06-29 12:44:07 +0100

    fix dashboard room item private detail

--- src/components/dashboard/dashboard-room-item.tsx
+++ src/components/dashboard/dashboard-room-item.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import IRoomInfo from "@/interfaces/room-info";
+import { IRoomInfo } from "@/interfaces/room-info";
 import { cn } from "@/lib/utils";
 
-export default function DashboardRoomItem({roomData, joinRoom}: {roomData: IRoomInfo, joinRoom: (id: number) => void}) {
+export default function DashboardRoomItem({ roomData, joinRoom }: { roomData: IRoomInfo, joinRoom: (id: number) => void }) {
   return (
     <div className={cn("w-[98%] h-[100%] bg-black rounded p-2 flex flex-col")}>
       <div className={cn("w-full h-[10%] flex justify-center items-center")}>
@@ -13,7 +13,7 @@ export default function DashboardRoomItem({roomData, joinRoom}: {roomData: IRoom
         <p className={cn("md:text-base sm:text-sm")}><span className={cn("font-bold")}>Topic:</span> {roomData.topic}</p>
         <p className={cn("md:text-base sm:text-sm")}><span className={cn("font-bold")}>Difficulty:</span> {roomData.difficulty}</p>
         <p className={cn("md:text-base sm:text-sm")}><span className={cn("font-bold")}>Players:</span> {roomData.currentPlayers}/{roomData.maxPlayers}</p>
-        <p className={cn("md:text-base sm:text-sm")}><span className={cn("font-bold")}>Private:</span> {roomData.open ? "No": "Yes"}</p>
+        <p className={cn("md:text-base sm:text-sm")}><span className={cn("font-bold")}>Private:</span> {!roomData.open ? "No" : "Yes"}</p>
       </div>
       <div className={cn("w-full h-[20%] flex justify-center items-center")}>
         <button onClick={() => joinRoom(roomData.id)} className={cn("w-[100px] h-[100%] rounded sm:text-sm bg-green-700 text-base font-bold hover:bg-green-900 transition-colors disabled:cursor-not-allowed disabled:bg-gray-700 inline-flex justify-center items-center")} disabled={roomData.currentPlayers >= roomData.maxPlayers}>Join room</button>
